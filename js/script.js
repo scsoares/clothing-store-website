753be1accacf4e05c7607f645e3a2b0df1dbf9b6
scsoares <sarahcsf2@gmail.com>
2024-01-24 02:35:34 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 function initialize() {
-    const NAV_TOGGLE = document.querySelector(".nav-toggle");
-    NAV_TOGGLE.addEventListener("click", toggleNavbarMobile);
+  const NAV_TOGGLE = document.querySelector(".nav-toggle");
+  NAV_TOGGLE.addEventListener("click", toggleNavbarMobile);
 
-    const NAV_HIDE = document.querySelector(".nav-hide");
-    NAV_HIDE.addEventListener("click", hideNavbarMobile);
-  }
-  
-  const NAVBAR_MOBILE = document.querySelector(".navbar-mobile-sidebar");
-  // Toggle mobile sidebar visibility
-  function toggleNavbarMobile() {
-    NAVBAR_MOBILE.classList.toggle("active");
-  }
+  const NAV_HIDE = document.querySelector(".nav-hide");
+  NAV_HIDE.addEventListener("click", hideNavbarMobile);
 
-  function hideNavbarMobile() {
-    NAVBAR_MOBILE.classList.remove("active");
+  // Add an event listener for scroll to trigger the animation
+  document.addEventListener('scroll', handleScroll);
+}
+
+const NAVBAR_MOBILE = document.querySelector(".navbar-mobile-sidebar");
+
+// Toggle mobile sidebar visibility
+function toggleNavbarMobile() {
+  NAVBAR_MOBILE.classList.toggle("active");
+}
+
+function hideNavbarMobile() {
+  NAVBAR_MOBILE.classList.remove("active");
+}
+
+// Handle scroll to trigger the animation for new arrivals
+function handleScroll() {
+  const newArrivals = document.querySelector('.new-arrivals');
+  const scrollPosition = window.scrollY;
+
+  // Adjust the threshold as needed
+  if (scrollPosition < newArrivals.offsetTop) {
+    newArrivals.classList.add("animate-in");
+  } else {
+    newArrivals.classList.add("animate-out");
   }
-  
-  initialize();
+}
+
+initialize();
